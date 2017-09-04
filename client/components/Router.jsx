607985ef76ae74392a782e313faa95231457b9bf
олег ростов <terabyte.or@gmail.com>
2017-09-04 20:48:36 +0300

    add login form

--- client/components/Router.jsx
+++ client/components/Router.jsx
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './App';
 import Greetings from './Greetings';
 import SignupPage from './signup/SignupPage';
+import LoginPage from './login/LoginPage';
 
 const MRouter = () => <Router>
 	<App>
 		<Route exact path="/" component={Greetings} />
 		<Route path="/signup" component={SignupPage} />
+		<Route path="/login" component={LoginPage} />
 	</App>
 </Router>;
 
---
+++ client/components/login/LoginForm.jsx
@@ -0,0 +1,93 @@
+import React, {Component} from 'react';
+import TextFieldGroup from '../common/TextFieldGroup';
+import validateInput from '../../../server/shared/validations/login';
+import {connect} from 'react-redux';
+import {login} from '../../actions/login';
+import PropTypes from 'prop-types';
+import {addFlashMessages} from '../../actions/flashMessages';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      identifier: '',
+      password: '',
+      errors: {},
+      isLoading: false
+    };
+
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  isValid() {
+		const {errors, isValid} = validateInput(this.state);
+
+		if (!isValid) {
+			this.setState({errors});
+		}
+		return isValid;
+	}
+
+  handleOnSubmit(e) {
+    e.preventDefault();
+    if (this.isValid()) {
+			this.setState({errors: {}, isLoading: true});
+			this.props.login(this.state)
+					.then((res) => {
+						addFlashMessages({
+							type: 'success',
+							text: 'You have logined up successfully! Welcome!'
+						});
+						this.context.router.history.push('/');
+					})
+					.catch(
+						(error) => this.setState({errors: error.response.data, isLoading: false})
+					);
+		}
+  }
+
+  render() {
+    const {identifier, password, errors, isLoading} = this.state;
+    return (
+      <form onSubmit={this.handleOnSubmit}>
+        <h1>Please Login</h1>
+        <TextFieldGroup
+          error={errors.identifier}
+          label="Username / E-mail"
+          onChange={this.handleOnChange}
+          value={identifier}
+          field="identifier"
+        />
+        <TextFieldGroup
+          error={errors.password}
+          label="Password"
+          onChange={this.handleOnChange}
+          value={password}
+          field="password"
+          type="password"
+        />
+        <div className="form-group">
+          <button
+            disabled={isLoading}
+            className="btn btn-primary btn-lg">Login</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+  login: PropTypes.func.isRequired,
+  addFlashMessages: PropTypes.func.isRequired
+}
+
+LoginForm.contextTypes = {
+  router: PropTypes.object.isRequired
+}
+
+export default connect(null, {login, addFlashMessages})(LoginForm);
